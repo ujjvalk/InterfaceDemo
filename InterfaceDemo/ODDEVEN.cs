ba4a4f2ade3b69cbfa86f81ca9ab616b56f55659
ujjvalk <ujjvalk@gmail.com>
2016-10-06 15:00:09 +0530

    Add project files.

---
+++ InterfaceDemo/ODDEVEN.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    interface IOne
+    {
+        void One();
+    }
+
+    interface ITwo
+    {
+        void Two();
+    }
+    interface IThree : IOne
+    {
+        void Three();
+    }
+    interface IFour : ITwo
+    {
+        void Four();
+    }
+    interface IFive : IThree
+    {
+        void Five();
+    }
+    interface IEVEN : ITwo, IFour
+    {
+        void Even();
+    }
+    interface IODD:IFive
+    {
+      void Odd();
+    }
+
+    public class ODDEVEN : IEVEN, IODD
+{
+        public void Even()
+        {
+            Console.WriteLine("\n\nThis is Even.\n");
+            var j = new ODDEVEN();
+            j.Two();
+            j.Four();
+        }
+
+        public void Five()
+        {
+            Console.WriteLine("This is five.");
+        }
+
+        public void Four()
+        {
+            Console.WriteLine("This is four.");
+        }
+
+        public void Odd()
+        {
+            Console.WriteLine("This is Odd.\n");
+        }
+
+        public void One()
+        {
+            Console.WriteLine("This is One.");
+        }
+
+        public void Three()
+        {
+            Console.WriteLine("This is Three.");
+        }
+
+        public void Two()
+        {
+            Console.WriteLine("This is Two.");
+        }
+    }
+}
---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InterfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IODD obj1 = new ODDEVEN();
+            obj1.Odd();
+            obj1.One();
+            obj1.Three();
+            obj1.Five();
+
+            var obj2 = new ODDEVEN();
+            obj2.Even();
+
+            //IFive notuseOdd = new ODDEVEN();
+            //notuseOdd.Odd();//Error on .Odd
+            Console.ReadLine();
+        }
+    }
+}
